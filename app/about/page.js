9392e6613226d1454d613fc216b7d93d439651a6
Veljko <veljko85varvarin@hotmail.com>
2023-05-09 20:56:42 +0200

    added about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,9 @@
 import styles from './page.module.css'
 
-export default function Home() {
+export default function About() {
   return (
     <main className={styles.main}>
-      <h1>Home Quince Media</h1>
+      <h1>About Qunce Media</h1>
     </main>
   )
 }
