466fef12507cedc97be5489835f14de5218ebd5d
Veljko <veljko85varvarin@hotmail.com>
2023-05-11 13:56:36 +0200

    nav bar

--- app/about/page.js
+++ app/about/page.js
@@ -4,8 +4,8 @@ import styles from './page.module.css'
 export default function About() {
   return (
     <main className={styles.main}>
-      <Heading/>
-      <h1>About Qunce Media</h1>
+      {/* <Heading/> */}
+      <h1>About Quince Media</h1>
       </main>
   )
 }
